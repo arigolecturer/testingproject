df6ff97edf1de8fe17f0254047c820ce076f3e01
arigo.lecturer <arigo.lecturer@gmail.com>
2024-10-03 04:00:59 +0000

    first commit

---
+++ src/app/page.js
@@ -0,0 +1,50 @@
+import Image from "next/image";
+
+export default function Home() {
+  let variabelA = 22;
+
+  return (
+    <div>
+      <table>
+        <tr>
+          <th>Company</th>
+          <th>Contact</th>
+          <th>Country</th>
+        </tr>
+        <tr>
+          <td>Alfreds Futterkiste</td>
+          <td>Maria Anders</td>
+          <td>Germany</td>
+        </tr>
+        <tr>
+          <td>Centro comercial Moctezuma</td>
+          <td>Francisco Chang</td>
+          <td>Mexico</td>
+        </tr>
+        <tr>
+          <td>Ernst Handel</td>
+          <td>Roland Mendel</td>
+          <td>Austria</td>
+        </tr>
+        <tr>
+          <td>Island Trading</td>
+          <td>Helen Bennett</td>
+          <td>UK</td>
+        </tr>
+        <tr>
+          <td>Laughing Bacchus Winecellars</td>
+          <td>Yoshi Tannamuri</td>
+          <td>Canada</td>
+        </tr>
+        <tr>
+          <td>Magazzini Alimentari Riuniti</td>
+          <td>Giovanni Rovelli</td>
+          <td>Italy</td>
+        </tr>
+      </table>
+
+      <p>I am normal</p>
+      <p style={{ color: '#4287f5', fontSize: '30px' }}>Nilai ini adalah {variabelA}</p>
+    </div>
+  );
+}
